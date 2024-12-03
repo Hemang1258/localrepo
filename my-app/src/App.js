d2a88ffcd87ea703a75accb0b3d17e6e7ef06e46
Hemang1258 <barothemang963@gmail.com>
2024-12-03 15:19:37 +0530

    Project File

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+//import img from './hemang.jpg.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 className="head">Happy Tail</h1>
+      
+    <nav className="navbar">
+      
+    <div className="navbar-container">
+      <a href="/" >Home</a>
+         <a href="/" >Category</a>
+      <a href="/" >Doctor</a>
+    <a href="/" >Trainer</a>
+  </div>
+  <div className="navbar-containers">
+    <a href="/">Sign up</a>
+    </div>
+   </nav> 
+   </> 
+  );
+}
+
+export default App;
